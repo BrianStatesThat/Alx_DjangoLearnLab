a9e7a2683c517fc3056e63d62a6dbce3f6b63b2e
brianstatesthat <mbspeelman@gmail.com>
2025-09-11 21:33:47 +0200

    starting over

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Book, Library
 
-# Create your views here.
+def list_books(request):
+    """Function-based view to list all books"""
+    books = Book.objects.all().select_related('author')
+    return render(request, 'relationship_app/list_books.html', {'books': books})
