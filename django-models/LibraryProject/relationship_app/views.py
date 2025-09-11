ccebdd9ebcbc18cadb2e06bf35ea851f053ec320
brianstatesthat <mbspeelman@gmail.com>
2025-09-11 22:15:56 +0200

    rock with me now

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,5 @@
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render
 from django.views.generic.detail import DetailView
 from .models import Book, Library
