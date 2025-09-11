7236d44c1dd9dbcd2c28a926d7a64bbc93360e0c
brianstatesthat <mbspeelman@gmail.com>
2025-09-11 22:18:59 +0200

    rock with me now

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,18 +1,29 @@
-from django.contrib.auth import login
+from django.contrib.auth import login, logout
 from django.contrib.auth.forms import UserCreationForm
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.detail import DetailView
 from .models import Book, Library
-from .models import Library   # <-- Added Library import
 
 def list_books(request):
     """Function-based view to list all books"""
     books = Book.objects.all().select_related('author')
     return render(request, 'relationship_app/list_books.html', {'books': books})
 
+def register(request):
+    """User registration view"""
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)  # <-- UserCreationForm()
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')  # Change 'home' to your homepage URL name
+    else:
+        form = UserCreationForm()  # <-- UserCreationForm()
+    return render(request, 'relationship_app/register.html', {'form': form})  # <-- register.html
+
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = "relationship_app/library_detail.html"  # <-- Required template
+    template_name = "relationship_app/library_detail.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
