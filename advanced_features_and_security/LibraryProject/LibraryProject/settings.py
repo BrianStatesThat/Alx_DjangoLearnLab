12fdd4471d5c86d5c7baec1fda280fc359864106
brianstatesthat <mbspeelman@gmail.com>
2025-09-14 10:53:20 +0200

    auth user model to settings file

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -124,6 +124,8 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
+AUTH_USER_MODEL = 'bookshelf.CustomUser'
+
 from django.conf import settings
 
 class UserProfile(models.Model):
