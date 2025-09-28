672d16e0a108bf9a6b5c3d494b778329da6c4215
brianstatesthat <mbspeelman@gmail.com>
2025-09-28 13:14:25 +0200

    idk

---
+++ advanced-api-project/advanced_api_project/api/urls.py
@@ -0,0 +1,19 @@
+"""
+URL configuration for API application.
+Defines endpoints for Author and Book resources.
+"""
+
+from django.urls import path
+from . import views
+
+app_name = 'api'
+
+urlpatterns = [
+    # Author endpoints
+    path('authors/', views.AuthorListCreateView.as_view(), name='author-list'),
+    path('authors/<int:pk>/', views.AuthorDetailView.as_view(), name='author-detail'),
+    
+    # Book endpoints
+    path('books/', views.BookListCreateView.as_view(), name='book-list'),
+    path('books/<int:pk>/', views.BookDetailView.as_view(), name='book-detail'),
+]
