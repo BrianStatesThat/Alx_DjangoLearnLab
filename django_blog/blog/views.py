65d138941bbbf348ec53b0a513fb8e0bc287f866
brianstatesthat <mbspeelman@gmail.com>
2025-10-05 18:28:44 +0200

    added templates amended models apps and views and urls and created forms and signals py file

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth import views as auth_views
+from django.contrib.auth.decorators import login_required
 # Create your views here.
+from .forms import CustomUserCreationForm
+
+def register(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'blog/register.html', {'form': form})
+@login_required
+def profile(request):
+    if request.method == 'POST':
+        request.user.email = request.POST.get('email')
+        request.user.save()
+    return render(request, 'blog/profile.html', {'user': request.user})
