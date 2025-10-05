3dbf723e3d169725424b5eb158f024ce18c59c84
brianstatesthat <mbspeelman@gmail.com>
2025-10-05 22:50:51 +0200

    rwmn

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -6,6 +6,9 @@ from .views import (
     PostUpdateView, PostDeleteView
 )
 from .views import CommentCreateView, CommentUpdateView, CommentDeleteView
+from .views import search_posts
+
+
 
 urlpatterns = [
     path('', views.index, name='index'),
@@ -24,4 +27,5 @@ path('post/<int:pk>/comments/new/', CommentCreateView.as_view(), name='comment-c
 # Comment update and delete by comment ID
 path('comment/<int:pk>/update/', CommentUpdateView.as_view(), name='comment-update'),
 path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment-delete'),
+path('search/', search_posts, name='search-posts'),
 ]
