1bcf0fba4f8d9f7b3236b93340aabfc2e80309f3
brianstatesthat <mbspeelman@gmail.com>
2025-10-05 22:45:45 +0200

    rwmn

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -5,6 +5,16 @@ from django.contrib.auth.models import User
 from .models import Post
 from .models import Comment
 
+from taggit.forms import TagWidget
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'content', 'tags']
+        widgets = {
+            'tags': TagWidget(),
+        }
+
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
@@ -19,8 +29,3 @@ class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ("username", "email", "password1", "password2")
-
-class PostForm(forms.ModelForm):
-    class Meta:
-        model = Post
-        fields = ['title', 'content']
