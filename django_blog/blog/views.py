ffd1914351dd87c2d173db05be9e134b7b87671f
brianstatesthat <mbspeelman@gmail.com>
2025-10-05 22:55:56 +0200

    rwmn

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -8,6 +8,21 @@ from django.urls import reverse_lazy
 from .models import Post, Comment
 from django.db.models import Q
 from .models import Post
+from taggit.models import Tag
+
+class PostByTagListView(ListView):
+    model = Post
+    template_name = 'blog/tag_posts.html'
+    context_object_name = 'posts'
+
+    def get_queryset(self):
+        self.tag = Tag.objects.get(slug=self.kwargs['tag_slug'])
+        return Post.objects.filter(tags__in=[self.tag])
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tag'] = self.tag
+        return context
 
 def search_posts(request):
     query = request.GET.get('q')
