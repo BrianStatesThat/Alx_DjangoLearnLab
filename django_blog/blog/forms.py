74344e3ba6fd8cd9acdeb7d3b01ec0e6453fbc07
brianstatesthat <mbspeelman@gmail.com>
2025-10-05 21:55:46 +0200

    rwn

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -3,6 +3,15 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Post
+from .models import Comment
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['content']
+        widgets = {
+            'content': forms.Textarea(attrs={'rows': 3}),
+        }
 
 class CustomUserCreationForm(UserCreationForm):
     email = forms.EmailField(required=True)
